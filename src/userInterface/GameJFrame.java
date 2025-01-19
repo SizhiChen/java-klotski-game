f952b1ef776e130e28c2ea30de42dc0f16a12a02
SizhiChen <chen.sizhi@hotmail.com>
2025-01-19 13:26:47 -0800

    Create different JFrame

---
+++ src/userInterface/GameJFrame.java
@@ -0,0 +1,10 @@
+package userInterface;
+
+import javax.swing.*;
+
+/**
+ * This class is a JFrame that represents the game window.
+ */
+public class GameJFrame extends JFrame {
+
+}
---
+++ src/userInterface/LoginJFrame.java
@@ -0,0 +1,9 @@
+package userInterface;
+
+import javax.swing.*;
+
+/**
+ * This class is a JFrame that represents the login window.
+ */
+public class LoginJFrame extends JFrame {
+}
